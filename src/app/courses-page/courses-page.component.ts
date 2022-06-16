69c0dad22d4b91512a0d74635bb047b2a604724b
Taras <kobetiakt@gmail.com>
2022-06-16 20:38:07 +0300

    1

--- src/app/courses-page/courses-page.component.ts
+++ src/app/courses-page/courses-page.component.ts
@@ -1,17 +1,26 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CoursePage } from '../interfaces/classes';
+
 
 @Component({
   selector: 'app-courses-page',
   templateUrl: './courses-page.component.html',
   styleUrls: ['./courses-page.component.scss']
 })
-export class CoursesPageComponent {
+export class CoursesPageComponent implements OnInit {
 
   @Input() course: CoursePage;
   @Output() deleteClicked: EventEmitter<number> = new EventEmitter();
   @Output() editClicked: EventEmitter<void> = new EventEmitter();
 
+  upcoming: boolean = false;
+  fresh: boolean = false;
+  milisecToDay: number = 1000 * 60 * 60 * 24;
+
+  ngOnInit(): void {
+    this.createFreshBorder()
+    this.createUpcomingBorder()
+  }
 
   onDeleteClicked() {
     this.deleteClicked.emit(this.course.id)
@@ -19,8 +28,26 @@ export class CoursesPageComponent {
 
   onEditClicked() {
     this.editClicked.emit()
+    this.createUpcomingBorder()
   }
 
+  createFreshBorder(): void {
+    if ((+this.course.creationDate - +new Date()) / this.milisecToDay > -14 &&
+      +this.course.creationDate - +new Date() < 0) {
+
+      console.log((+this.course.creationDate - +new Date()) / this.milisecToDay);
+
+
+      this.fresh = true;
+    }
+  }
+
+  createUpcomingBorder(): void {
+    if ((+this.course.creationDate - +new Date()) > 0) {
+      this.upcoming = true;
+    }
+
+  }
 
 }
 
--- src/app/pages-block/pages-block.component.ts
+++ src/app/pages-block/pages-block.component.ts
@@ -14,8 +14,6 @@ export class PagesBlockComponent implements OnInit {
   }
 
   deleteComponent(id: number): void {
-    // this.courses.splice(this.courses.findIndex(el => el.id === id), 1)
-
     this.courses = this.courses.filter(el => el.id !== id)
   }
 
@@ -25,9 +23,9 @@ export class PagesBlockComponent implements OnInit {
 
   ngOnInit(): void {
     this.courses = [
-      new CoursePage(1, 'SomeTitle1', new Date(), 1, 'Some description Lorem ipsum dolor sit amet dolor sit amet consectetur consectetur dolor sit amet consectetur, adipisicing elit. Asperiores, est! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, itaque.'),
-      new CoursePage(2, 'SomeTitle2', new Date(), 10, 'Not good course.'),
-      new CoursePage(3, 'SomeTitle3', new Date(), 12, 'Some description Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, est! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, itaque.'),
+      new CoursePage(1, 'SomeTitle1', new Date(2022, 5, 3), 1, 'Some description Lorem ipsum dolor sit amet dolor sit amet consectetur consectetur dolor sit amet consectetur, adipisicing elit. Asperiores, est! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, itaque.'),
+      new CoursePage(2, 'SomeTitle2', new Date(2022, 5, 21), 10, 'Not good course.'),
+      new CoursePage(3, 'SomeTitle3', new Date(2021, 5, 3), 12, 'Some description Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores, est! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, itaque.'),
     ]
   }
 }
