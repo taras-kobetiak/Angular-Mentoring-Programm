258a8c7f7671b90ef829155bd8bf29f2f696ad02
Taras <kobetiakt@gmail.com>
2022-06-27 15:27:08 +0300

    remove getter from classes.ts

--- src/app/interfaces/classes.ts
+++ src/app/interfaces/classes.ts
@@ -2,13 +2,7 @@ import { ICoursePage } from "./course.interface";
 import { IUserEntyty } from "./user-entyty.interface";
 
 export class CoursePage implements ICoursePage {
-    // public get description(): string {
-    //     return this._description;
-    // }
-    // public set description(value: string) {
-    //     if (value.length > 190)
-    //         this._description = value.slice(0, 189) + '...';
-    // }
+
     constructor(public id: number,
         public title: string,
         public creationDate: Date,
