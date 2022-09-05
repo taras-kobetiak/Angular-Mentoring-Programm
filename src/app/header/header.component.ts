4fad63ffb94998154a44f3cd76d3fee15b2ffb40
Taras <kobetiakt@gmail.com>
2022-09-05 15:19:01 +0300

    header bug

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IUserEntyty } from '../interfaces/user-entyty.interface';
 import { AuthServiceService } from '../authentication/services/auth-service.service';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { filter, Observable, Subject, switchMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -11,25 +11,34 @@ import { Observable, Subject, takeUntil } from 'rxjs';
 export class HeaderComponent implements OnInit, OnDestroy {
 
   currentUser: IUserEntyty;
-  isAuth$: Observable<boolean>;
+  isAuth: boolean;
   private unsubscribingData$: Subject<void> = new Subject<void>();
 
   constructor(public authService: AuthServiceService) { };
 
   ngOnInit(): void {
-    this.isAuth$ = this.authService.isAuthenticated();
+    this.authService.isAuthenticated().pipe(
+      takeUntil(this.unsubscribingData$),
+      filter((value: boolean) => {
+        this.isAuth = value;
+        return value !== false;
+      }),
+      switchMap(() => {
+        let currentUserData: string | null = localStorage.getItem('currentUser');
+        let currentUserEmail: string = currentUserData ? JSON.parse(currentUserData) : '';
+        return this.authService.getUserInfo(currentUserEmail);
+      }),
 
-    let currentUserData: string | null = localStorage.getItem('currentUser');
-    let currentUserEmail: string = currentUserData ? JSON.parse(currentUserData) : '';
+    )
+      .subscribe((val: IUserEntyty[]) => {
 
-    this.authService.getUserInfo(currentUserEmail).pipe(
-      takeUntil(this.unsubscribingData$)
-    ).subscribe((val: IUserEntyty[]) => {
-      this.currentUser = val[0];
-
-      console.log(this.currentUser);
-    })
+        this.currentUser = val[0];
+        console.log(this.currentUser);
+      })
   }
+
+
+
 
   onLogOutClick(): void {
     this.authService.logOut();
