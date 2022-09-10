2f1a8046c680bc86e86edffee084eecb33e172f3
Taras <kobetiakt@gmail.com>
2022-09-10 19:11:38 +0300

    resolve conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthServiceService } from './authentication/services/auth-service.service';
 import { LoadingService } from './shared/loading-block/servises/loading.service';
-
-=======
-import { Component, DoCheck } from '@angular/core';
-import { AuthServiceService } from './authentication/services/auth-service.service';
->>>>>>> main
 
 @Component({
   selector: 'app-root',
@@ -22,15 +16,10 @@ export class AppComponent implements OnInit {
   isAuth$: Observable<boolean>;
   isLoading$: Observable<boolean>;
 
-<<<<<<< HEAD
   constructor(private authService: AuthServiceService, private loadingService: LoadingService) { }
 
   ngOnInit(): void {
     this.isLoading$ = this.loadingService.getIsLoadingValue()
     this.isAuth$ = this.authService.isAuthenticated();
-=======
-  ngDoCheck(): void {
-    this.isAuth = this.authService.isAuthenticated();
->>>>>>> main
   }
 }
