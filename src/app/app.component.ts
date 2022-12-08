a62a20aaec75c8d0017ad81ff3221f6f453ce12e
Taras <kobetiakt@gmail.com>
2022-12-08 19:36:08 +0200

    add logOut to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { isLoadingSelector } from './store/reducers/isLoading.reducer';
 import { isLoginSelector } from './store/reducers/isAuth.reducer';
 import { AuthServiceService } from './authentication/services/auth-service.service';
+
 
 @Component({
   selector: 'app-root',
@@ -11,9 +12,20 @@ import { AuthServiceService } from './authentication/services/auth-service.servi
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnDestroy, OnInit {
   isAuth$: Observable<boolean> = this.store.select(isLoginSelector);
   isLoading$: Observable<boolean> = this.store.select(isLoadingSelector);
 
-  constructor(private store: Store) { }
+  constructor(
+    private store: Store,
+    private authService: AuthServiceService
+  ) { }
+  ngOnInit(): void {
+    console.log(localStorage);
+
+  }
+
+  ngOnDestroy(): void {
+    this.authService.logOut()
+  }
 }
--- src/app/store/actions/isAuth.action.ts
+++ src/app/store/actions/isAuth.action.ts
@@ -2,3 +2,4 @@ import { createAction } from '@ngrx/store'
 
 export const isAuthHeaderFalse = createAction('[HEADER ISLOGIN] false');
 export const isAuthLoginPageTrue = createAction('[LOGINPAGE ISLOGIN] true');
+
